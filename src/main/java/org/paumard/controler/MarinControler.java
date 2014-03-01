fcaa063d393204cb874d3b08806bd22ec0b571e2
hammoum <kar.hammoum@gmail.com>
2014-03-01 21:45:57 +0100

    Update MarinControler.java
    ajout delete-prenom

--- src/main/java/org/paumard/controler/MarinControler.java
+++ src/main/java/org/paumard/controler/MarinControler.java
@@ -31,6 +31,13 @@ public class MarinControler implements Serializable {
 		System.out.println("Marin persisté : " + marin.getNom()) ;
 		
 	}
+	@SuppressWarnings("unchecked")
+        public void deletePrenom() {
+		
+		marins= marinEJB.deleteMarinByPrenom(marin);
+		System.out.println("Marin persisté : " + marin.getPrenom()) ;
+		
+	}
 	public Marin getMarin() {
 		return marin;
 	}
